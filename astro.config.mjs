d0506237d8d85ce226c1b267f2c44fd428ee1fe5
NakedSnakeMG3 <santiago.castaneda.ramos@gmail.com>
2023-11-14 17:38:09 -0600

    SCR remove base url of astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://petsdnc.github.io',
-    base: '/pets-dogs-n-cats',
 });
